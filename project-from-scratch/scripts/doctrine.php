79bc44b564da6e0d768561928087593d8c5cdcdf
Roy Simkes <roysimkes@gmail.com>
2010-07-22 11:02:44 +0300

    added additional model creation to the modules allowing them to be added and removed by their installation, another feature for 'pluggable' modules

--- project-from-scratch/scripts/doctrine.php
+++ project-from-scratch/scripts/doctrine.php
@@ -3,14 +3,28 @@ require("public/header.php");
 
 $application->bootstrap();
 
+$defaultPath = APPLICATION_PATH;
+
+$args = $_SERVER["argv"];
+
+//Perhaps, I want to create models and tables or create sqls for just a module!!!
+//Usage: ./doctrine -m MODULE_NAME generate-models-yaml
+//The above usage will only generate models for the yaml defined in the module directory
+$moduleNameIndex = array_search("-m", $args);
+if ($moduleNameIndex !== false) { //Check if the module bootstrapping exists or not...
+    $defaultPath = APPLICATION_PATH . "/modules/" . $args[$moduleNameIndex+1];
+    //Now unset these module variables to prevent causing problems in the doctrine command line
+    array_splice($args, $moduleNameIndex, 2);
+}
+
 // Configure Doctrine Cli
 // Normally these are arguments to the cli tasks but if they are set here the arguments will be auto-filled
 $config = array(
-    'data_fixtures_path'  =>  APPLICATION_PATH . '/data/fixtures',
-    'models_path'         =>  APPLICATION_PATH . '/models/doctrine/',
-    'migrations_path'     =>  APPLICATION_PATH . '/data/migrations',
-    'sql_path'            =>  APPLICATION_PATH . '/data/sql',
-    'yaml_schema_path'    =>  APPLICATION_PATH . '/data/schema',
+    'data_fixtures_path'  =>  $defaultPath . '/data/fixtures',
+    'models_path'         =>  $defaultPath . '/models/doctrine/',
+    'migrations_path'     =>  $defaultPath . '/data/migrations',
+    'sql_path'            =>  $defaultPath . '/data/sql',
+    'yaml_schema_path'    =>  $defaultPath . '/data/schema',
     "generate_models_options" => array(
         "phpDocPackage"       => "Kartaca",
         "phpDocSubpackage"    => "Doctrine",
@@ -21,4 +35,5 @@ $config = array(
 );
 
 $cli = new Doctrine_Cli($config);
-$cli->run($_SERVER['argv']);
+$cli->run($args);
+
